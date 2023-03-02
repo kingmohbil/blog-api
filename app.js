843bc5c2d80837042b2335e6f18692b907d30b24
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-02 21:06:50 +0300

    The API routes were added to the app.js

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ require('dotenv').config();
 const debug = require('debug')('server');
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
+const initializePassport = require('./passport-config');
 const { connect } = require('./connection');
-const server = express();
 const signupRoute = require('./routes/signup-route');
+const loginRoute = require('./routes/login-route');
+const postsRoute = require('./routes/posts-route');
+const server = express();
+initializePassport(passport);
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(cors());
-
+server.use(passport.initialize());
+server.use(methodOverride('_method'));
 server.use('/', signupRoute);
-
+server.use('/', loginRoute);
+server.use('/', postsRoute);
 server.listen(process.env.PORT, () => {
   debug(`server listening on port ${process.env.PORT}...`);
 });
